8be7b857bd74cfb1aec0e2a2b81f69564b6091e8
Celirbis <rina-ashino@yandex.ru>
2022-08-27 12:00:00 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+
+function DateTime(props) {
+    return (
+        <p className="date">{props.date}</p>
+    )
+}
+
+function Video(props) {
+    return (
+        <div className="video">
+            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <DateTimePretty date={props.date} />
+        </div>
+    )
+}
+
+function VideoList(props) {
+    return props.list.map(item => <Video url={item.url} date={item.date} />);
+}
+
+function doPretty(Component) {
+    return function (props) {
+        const minutesPassed = (new Date() - new Date(props.date)) / 1000 / 60;
+        let prettyDate;
+
+        if (minutesPassed < 60) prettyDate = `${Math.trunc(minutesPassed)} минут назад`;
+        else if (minutesPassed < 60 * 24) prettyDate = `${Math.trunc(minutesPassed / 60)} часов назад`;
+        else prettyDate = `${Math.trunc(minutesPassed / 60 / 24)} дней назад`;
+
+        return (
+            <>
+                <Component date={props.date} />
+                <p className="date">{"(" + prettyDate + ")"}</p>
+            </>
+        )
+    }
+}
+
+const DateTimePretty = doPretty(DateTime);
+
+export default function App() {
+    const [list, setList] = useState([
+        {
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2017-07-31 13:24:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-03-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2022-02-03 23:16:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2022-08-27 11:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2022-08-26 16:17:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2017-12-02 05:24:00'
+        },
+    ]);
+
+    return (
+        <VideoList list={list} />
+    );
+}
